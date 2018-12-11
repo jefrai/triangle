1b076b495d55761c6719b6cb1ce148f8d7823eea
jefrai <30693850+jefrai@users.noreply.github.com>
2018-12-11 23:58:41 +0800

    Add files via upload

---
+++ random.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct query {
+    expr lhs;
+    ieq op;
+    expr rhs;
+} query;
+
+typedef struct  {
+
+} op;
+
+
+
+int main() {
+
+}
+
+/*
+
+*/
