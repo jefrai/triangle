58b5a18d494eae898a5c66ad787280a24f15de94
jefrai <30693850+jefrai@users.noreply.github.com>
2018-12-12 01:54:33 +0800

    Add files via upload

--- random.cpp
+++ random.cpp
@@ -1,22 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef struct {
+
+} opr;
+
+typedef struct {
+
+} tri;
+
+typedef struct {
+
+} intr;
+
+typedef struct {
+
+} ratl;
+
+typedef struct {
+
+} radi;
+
+typedef struct expr {
+    expr *l;
+    opr op;
+    expr *r;
+} expr;
+
+typedef struct ineq {
+    int type;
+} ineq;
+
 typedef struct query {
     expr lhs;
-    ieq op;
+    ineq op;
     expr rhs;
 } query;
-
-typedef struct  {
-
-} op;
-
-
 
 int main() {
 
 }
-
-/*
-
-*/
