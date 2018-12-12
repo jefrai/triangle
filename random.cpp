5434b2eb2208042b5e2870d01b2629969c491d4d
jefrai <30693850+jefrai@users.noreply.github.com>
2018-12-13 01:42:59 +0800

    Add files via upload

--- random.cpp
+++ random.cpp
@@ -1,42 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef struct {
+/*
+0   ADDITION
+1   SUBTRACTION
+2   MULTIPLICATION
+3   DIVISION
+4   EXPONENTIATION
+5   EQUALITY
+6   LESS OR EQUAL
+7   GREATER OR EQUAL
+8   LESS THAN
+9   GREATER THAN
+10  INEQUALITY
+*/
 
-} opr;
+/*
+0   QUERY
+1   INEQUALITY OPERATOR
+2   EXPRESSION - ARITHMETIC
+3   EXPRESSION - GEOMETRIC
+4   EXPRESSION - EXPONENTIAL
+5   RATIONAL POWER - ARITHMETIC
+6   RATIONAL POWER - GEOMETRIC
+7   RATIONAL POWER - EXPONENTIAL
+8   RATIONAL - ARITHMETIC
+9   RATIONAL - GEOMETRIC
+10  RATIONAL - EXPONENTIAL
+11  INTEGER - ARITHMETIC
+12  INTEGER - GEOMETRIC
+13  INTEGER - EXPONENTIAL
+14  BASE TRIANGLE TERM
+15  OPERATOR
+*/
 
-typedef struct {
+/*
+0   a
+1   b
+2   c
+3   sin A
+4   sin B
+5   sin C
+6   s - semiperimeter
+7   r - radius of circumcircle
+*/
 
-} tri;
+deque<int> dq;
+vector<int> type, data;
+vector<pair<int, pair<int, int> > > desc;
 
-typedef struct {
-
-} intr;
-
-typedef struct {
-
-} ratl;
-
-typedef struct {
-
-} radi;
-
-typedef struct expr {
-    expr *l;
-    opr op;
-    expr *r;
-} expr;
-
-typedef struct ineq {
-    int type;
-} ineq;
-
-typedef struct query {
-    expr lhs;
-    ineq op;
-    expr rhs;
-} query;
+inline string rf(string s, string a, string b) {for (int i = 0; (i = s.find(a, i)) < s.size(); i += b.size()) s.replace(i, a.size(), b); return s;}
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    int N, i;
+    string input;
+    getline(cin, input);
+    input = rf(input, " ", "");
+    input = rf(input, "(-", "(0-");
+    if (input[0] == '-') input = "0" + input;
+    N = input.size();
+    for (i = 0; i < N; ++i) {
 
+    }
 }
