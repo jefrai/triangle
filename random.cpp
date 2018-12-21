0e255a895be3108fb821d0bd8a37a02ba4f4fb71
jefrai <30693850+jefrai@users.noreply.github.com>
2018-12-22 01:17:05 +0800

    Add files via upload

--- random.cpp
+++ random.cpp
@@ -11,77 +11,135 @@ using namespace std;
 
 /*  0 - NEW POINT
 0   NEW     CREATE POINT (IDENTIFIER)
-1   NEWC    CREATE POINT (IDENTIFIER, COORDINATE IDENTIFIERS)
-2   NEWF    CREATE POINT (IDENTIFIER, ORIGIN COORDINATES)
+1   NEC     CREATE POINT (IDENTIFIER, COORDINATE IDENTIFIERS)
 */
 
 /*  1 - FINITE LOCUS CONSTRAINT
 0   ILL     INTERSECTION LINE-LINE (2, 2)
 1   ILC     INTERSECTION LINE-CIRCLE (2, 2)
-2   ILI     INTERSECTION LINE-CIRCLE IMMEDIATE (2, 1, 1I)
+//2   ILI     INTERSECTION LINE-CIRCLE IMMEDIATE (2, 1, 1I)
 3   ICC     INTERSECTION CIRCLE-CIRCLE (2, 2)
-4   ICI     INTERSECTION CIRCLE-CIRCLE IMMEDIATE (2, 1, 1I)
-5   IC2     INTERSECTION CIRCLE-CIRCLE DIIMMEIDATE (1, 1I, 1, 1I)
+//4   ICI     INTERSECTION CIRCLE-CIRCLE IMMEDIATE (2, 1, 1I)
+//5   IC2     INTERSECTION CIRCLE-CIRCLE DIIMMEIDATE (1, 1I, 1, 1I)
 5   MID     MIDPOINT (2)
 6   MIR     MIRROR OVER LINE (1, 2)
+7   CEN     CENTROID (3)
+8   INC     INCENTRE (3)
+9   CIR     CIRCUMCENTRE (3)
 */
 
 /*  2 - LINE LOCUS CONSTRAINT
 0   COL     COLLINEAR (2)
-1   COLI    COLLINEAR IMMEDIATE (1, 3I)
+//1   COLI    COLLINEAR IMMEDIATE (1, 3I)
 2   PARA    PARALLEL (1, 2)
 3   PER     PERPENDICULAR (1, 2)
 4   PBIS    PERPENDICULAR BISECTOR (2)
 5   ABIS    ANGLE BISECTOR (1, 2)
-6   TAN     TANGENT (3)
-7   TANI    TANGENT IMMEDIATE (2, 1I)
+6   TAN     TANGENT (3) SOURCE-CENTRE-RADIUS
+//7   TANI    TANGENT IMMEDIATE (2, 1I)
 */
 
 /*  3 - CIRCLE/RADIUS LOCUS CONSTRAINT
-0   RAD     RADIUS (1, 2)
-1   RADI    RADIUS IMMEDIATE (1, 1I)
+0   RAD     RADIUS (1, 2) CENTRE-RADIUS
+//1   RADI    RADIUS IMMEDIATE (1, 1I)
 2   CONC    CONCYCLIC (3)
 */
 
 /*  4 - OTHER
-0   NEQ     NONEQUAL POINTS (2)
+0   NEQ     NONEQUAL POINTS (1)
+1   ORI     POINT AS ORIGIN (0)
+2   AXI     POINT ON X-AXIS (0)
 */
 
-int nc; //number of coordinates
+int nfc, ori, axi; //number of coordinates, (0, 0) point, (x, 0) point
 set<char> apn; //alphanumeric characters
 vector<string> v; //vector
 vector<int> pv; //priority vector
-map<string, int> mi; //map from point identifier to index
-vector<pair<int, int> > ci; //coordinate indices of point
+map<string, int> mpi, mci; //map from point identifier to index
+vector<pair<int, int> > pcd; //coordinate indices of point
+deque<string> pl; //polynomials
+
+inline string rs(int i, bool d) {return "z(" + to_string(d ? pcd[i].second : pcd[i].first) + ")";}
 
 void create(vector<string> f) {
+    mpi[f[1]] = pcd.size();
     if (f[0] == "new") {
-        mi[f[1]] = ci.size();
-        ci.push_back(make_pair(nc, nc + 1));
-        nc += 2;
+        pcd.push_back(make_pair(nfc, nfc + 1));
+        nfc += 2;
     }
-    if (f[0] == "newc") {
-
-    }
-    if (f[0] == "newf") {
-
+    if (f[0] == "nec") {
+        if (mci.find(f[2]) != mci.end() || mci.find(f[3]) != mci.end()) {cout << "Invalid" << endl; return;}
+        mci[f[2]] = nfc;
+        mci[f[3]] = nfc + 1;
+        pcd.push_back(make_pair(nfc, nfc + 1));
+        nfc += 2;
     }
 }
 
 void finite(vector<string> f) {
+    int r[4], i;
+    if (f[0] == "ill") {
 
+    }
+    if (f[0] == "ilc") {
+
+    }
 }
 
 void linear(vector<string> f) {
+    int r[4], i;
+    if (f[0] == "col") {
+        for (i = 0; i < 3; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("(" + rs(r[0], 1) + " - " + rs(r[1], 1) + ") * (" + rs(r[2], 0) + " - " + rs(r[1], 0) + ") - (" + rs(r[2], 1) + " - " + rs(r[1], 1) + ") * (" + rs(r[0], 0) + " - " + rs(r[1], 0) + ")");
+    }
+    if (f[0] == "para") {
+        for (i = 0; i < 4; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("(" + rs(r[0], 1) + " - " + rs(r[1], 1) + ") * (" + rs(r[3], 0) + " - " + rs(r[2], 0) + ") - (" + rs(r[3], 1) + " - " + rs(r[2], 1) + ") * (" + rs(r[0], 0) + " - " + rs(r[1], 0) + ")");
+    }
+    if (f[0] == "per") {
+        for (i = 0; i < 4; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("(" + rs(r[0], 1) + " - " + rs(r[1], 1) + ") * (" + rs(r[3], 1) + " - " + rs(r[2], 1) + ") + (" + rs(r[0], 0) + " - " + rs(r[1], 0) + ") * (" + rs(r[3], 0) + " - " + rs(r[2], 0) + ")");
+    }
+    if (f[0] == "pbis") {
+        for (i = 0; i < 3; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("(2 * " + rs(r[0], 1) + " - " + rs(r[1], 1) + " - " + rs(r[2], 1) + ") * (" + rs(r[2], 1) + " - " + rs(r[1], 1) + ") + (2 * " + rs(r[0], 0) + " - " + rs(r[1], 0) + " - " + rs(r[2], 0) + ")(" + rs(r[2], 0) + " - " + rs(r[1], 0) + ")");
+    }
+    if (f[0] == "abis") { //POSSIBLE EXTERNAL BISECTOR
+        for (i = 0; i < 4; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("");
+        pl.back() += "((" + rs(r[2], 1) + " - " + rs(r[1], 1) + ") * " + rs(r[0], 0) + " + (" + rs(r[1], 1) + " - " + rs(r[2], 1) + ") * " + rs(r[0], 1) + " + " + rs(r[2], 0) + " * " + rs(r[1], 1) + " - " + rs(r[1], 0) + " * " + rs(r[2], 1) + ")^2 * ";
+        pl.back() += "((" + rs(r[3], 1) + " - " + rs(r[1], 1) + ")^2 + (" + rs(r[1], 0) + " - " + rs(r[3], 0) + ")^2) - ";
+        pl.back() += "((" + rs(r[3], 1) + " - " + rs(r[1], 1) + ") * " + rs(r[0], 0) + " + (" + rs(r[1], 1) + " - " + rs(r[3], 1) + ") * " + rs(r[0], 1) + " + " + rs(r[3], 0) + " * " + rs(r[1], 1) + " - " + rs(r[1], 0) + " * " + rs(r[3], 1) + ")^2 * ";
+        pl.back() += "((" + rs(r[2], 1) + " - " + rs(r[1], 1) + ")^2 + (" + rs(r[1], 0) + " - " + rs(r[2], 0) + ")^2)";
+    }
+    if (f[0] == "tan") {
 
+    }
 }
 
 void radial(vector<string> f) {
-
+    int r[5], i;
+    if (f[0] == "rad") {
+        for (i = 0; i < 3; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("(" + rs(r[0], 0) + " - " + rs(r[1], 0) + ")^2 + (" + rs(r[0], 1) + " - " + rs(r[1], 1) + ")^2 - (" + rs(r[2], 0) + " - " + rs(r[1], 0) + ")^2 + (" + rs(r[2], 1) + " - " + rs(r[1], 1) + ")^2");
+    }
+    if (f[0] == "conc") {
+        for (i = 0; i < 5; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        r[5] = pcd.size();
+        pcd.push_back(make_pair(nfc, nfc + 1));
+        nfc += 2;
+        pl.push_back("(" + rs(r[2], 0) + " - " + rs(r[5], 0) + ")^2 + (" + rs(r[2], 1) + " - " + rs(r[5], 1) + ")^2 - (" + rs(r[1], 0) + " - " + rs(r[5], 0) + ")^2 + (" + rs(r[1], 1) + " - " + rs(r[5], 1) + ")^2");
+        pl.push_back("(" + rs(r[3], 0) + " - " + rs(r[5], 0) + ")^2 + (" + rs(r[3], 1) + " - " + rs(r[5], 1) + ")^2 - (" + rs(r[1], 0) + " - " + rs(r[5], 0) + ")^2 + (" + rs(r[1], 1) + " - " + rs(r[5], 1) + ")^2");
+        pl.push_back("(" + rs(r[0], 0) + " - " + rs(r[5], 0) + ")^2 + (" + rs(r[0], 1) + " - " + rs(r[5], 1) + ")^2 - (" + rs(r[1], 0) + " - " + rs(r[5], 0) + ")^2 + (" + rs(r[1], 1) + " - " + rs(r[5], 1) + ")^2");
+    }
 }
 
 void other(vector<string> f) {
-
+    int r[2], i;
+    if (f[0] == "neq") {
+        for (i = 0; i < 2; ++i) {if (mpi.find(f[i + 1]) == mpi.end()) {cout << "Invalid" << endl; return;} r[i] = mpi[f[i + 1]];}
+        pl.push_back("((" + rs(r[1], 0) + " - " + rs(r[0], 0) + ")^2 + (" + rs(r[1], 1) + " - " + rs(r[0], 1) + ")^2) * z(" + to_string(nfc++) + ") - 1");
+    }
 }
 
 /*  TODO
@@ -89,6 +147,8 @@ void other(vector<string> f) {
 */
 
 int main() {
+    freopen("in.txt", "r", stdin);
+    ios_base::sync_with_stdio(0);
     string s;
     int N, i, j;
     for (i = 0; i < 10; ++i) apn.insert('0' + i);
@@ -110,5 +170,7 @@ int main() {
         if (v[0] == "ln") linear(w);
         if (v[0] == "rd") radial(w);
         if (v[0] == "ms") other(w);
+
+        for (i = 0; i < pl.size(); ++i) cout << pl[i] << endl;
     }
 }
